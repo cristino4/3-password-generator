c1ac7e813ec6bdf41f81ec762e7870af44cbd348
cristino4 <117871733+cristino4@users.noreply.github.com>
2023-01-17 21:36:00 -0800

    added some comments to js

--- Develop/script.js
+++ Develop/script.js
@@ -9,7 +9,7 @@ var numbersCriteria;
 var specialCharCriteria;
 var uppercaseCriteria;
 var lengthCriteria;
-
+//define the character options for the password
 var passwordLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o"
                       ,"p","q","r","s","t","u","v","w","x","y","z"];
 var passwordNumbers = ["0","1","2","3","4","5","6","7","8","9"];
@@ -17,21 +17,38 @@ var passwordSpecialChar = ["!","@","#","$","%","^","&","*","(",")","-","_","+","
 ,"`","~","{","}","[","]",";",":","'","<",">",",",".","?","/","|"];
 
 
+
+//using prompts, get the user's password criteria and check it
 function getCriteria() {
-
-  window.confirm("Please select OK or CANCEL to select an option");
-  lettersCriteria = window.confirm("Do you want the password to contain LETTERS?");
-  uppercaseCriteria = window.confirm("Do you want the letters to be in UPPERCASE?");
-  numbersCriteria = window.confirm("Do you want the password to contain NUMBERS?");
-  specialCharCriteria = window.confirm("Do you want the password to contain special characters?");
-  lengthCriteria = Number(window.prompt("Please enter the password length (at least 8 characters and no more than 128 characters)"))
+  var lcf = false;
+ 
+  lettersCriteria = window.confirm("Do you want the password to contain LETTERS? (OK = YES, CANCEL = NO)");
+  if (lettersCriteria == true){
+    uppercaseCriteria = window.confirm("Do you want the letters to be in UPPERCASE? (OK = YES, CANCEL = NO)");
+  }
+  numbersCriteria = window.confirm("Do you want the password to contain NUMBERS? (OK = YES, CANCEL = NO)");
+  specialCharCriteria = window.confirm("Do you want the password to contain special characters?(OK = YES, CANCEL = NO)");
   
+  lengthCriteria = Number(window.prompt("Please enter the password length (at least 8 characters and no more than 128 characters)"));
 
+  while (lcf == false){
+    if (lengthCriteria >=8 && lengthCriteria <= 128){
+        
+        lcf == true;
+        break;
+      
+    } else {
+      lengthCriteria = Number(window.prompt("Try Again! Please enter the password length (a number at least 8 and no more than 128 characters)"));
+    }
+
+  }
+  
 
   // console.log(lettersCriteria,uppercaseCriteria,numbersCriteria,specialCharCriteria,lengthCriteria)
   return [lettersCriteria,uppercaseCriteria,numbersCriteria,specialCharCriteria,lengthCriteria];
 }
 
+//generate the password 
 function generatePassword(lettersCriteria,numbersCriteria,
     uppercaseCriteria,specialCharCriteria,lengthCriteria) {
     var password = [];
@@ -77,22 +94,35 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-
+  var confirm = window.confirm("Please select OK to generate a password.");
+  if (confirm == false){
+    return 0;
+  }
   var criteria = getCriteria();
   lettersCriteria = criteria[0];
   numbersCriteria = criteria[1];
   specialCharCriteria= criteria[2];
   uppercaseCriteria= criteria[3];
   lengthCriteria= criteria[4];
-  // console.log("criteria"+ criteria);
-  // console.log("write password")
-  // console.log(lettersCriteria,uppercaseCriteria,numbersCriteria,specialCharCriteria,lengthCriteria)
+  console.log("this is the criteria " +lettersCriteria,uppercaseCriteria,numbersCriteria,specialCharCriteria,lengthCriteria)
   var password = generatePassword(lettersCriteria,uppercaseCriteria,numbersCriteria,
     specialCharCriteria,lengthCriteria);
+  
+
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
+  // var lettersCritElement = document.querySelector("#letters").innerHTML;
+  // // var uppercaseCritElement = document.querySelector("#uppercase");
+  // // var numbersCritElement = document.querySelector("#numbers");
+  // // var specialCharCritElement = document.querySelector("#specialCharacters");
+  // // var lengthElement = document.querySelector("#lengths");
+ 
+  // console.log("value " + lettersCritElement.value);
+  // lettersCritElement.value = lettersCriteria;
+  // // uppercaseCritElement.value = uppercaseCriteria;
+  // // numbersCritElement.value = numbersCriteria;
+  // // specialCharCritElement.value = specialCharCriteria;
+  // // lengthElement.value = lengthCriteria;
 }
 
 // Add event listener to generate button
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Password Generator Starter Code
+# Challenge #3: Random Password Generator Application
+
+## Description
+This applicaiton creates a random password for the user. A user is prompted several questions to define the password criteria. After all the criteria is selected, the applicaiton creates a random password and displays it on the page. 
+
+## Accessing the Application
+The application can be accessed on the GitHub repo here: https://github.com/cristino4/3-password-generator/
+
+The applicaiton is also available on its published site: https://cristino4.github.io/3-password-generator/
+
+To run the application locally, download the applications files and open the index.html file on any web browser.
+
+## Screenshot of the application
+![Screenshot of the application](/assets/images/image1.png)
+![Screenshot of the application](/assets/images/image2.png)
